01569159687ffa09278b1f438552de5dde9d20f9
AJ Genung <DragonRydrz@me.com>
2018-06-29 13:00:25 -0400

    changes

--- djorg_bookmarks/settings.py
+++ djorg_bookmarks/settings.py
@@ -129,8 +129,12 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'djorg_bookmarks/static'),
+)
 
 REST_FRAMEWORK = {
     # Use Django's standard `django.contrib.auth` permissions,
